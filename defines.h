68e225ce67f59929af52a0c42d0a37387a383ce3
dbills <bills442@yahoo.com>
2016-12-21 18:17:41 -0500

    added diagrams for pinouts to controls

--- defines.h
+++ defines.h
@@ -1,3 +1,67 @@
+// UNO prototype 1 board connections when facing board hat
+/*   ________________________
+    /                        |
+    |                        |
+    |      C2/4              |
+    | C3/4                   |
+    |      C1/4              |
+    
+    
+    C1 yellow/white/black/black
+    C2 motor controller
+    C3 rocker Switvh green/yellow/white/blue
+    C3-1 : Gnd
+    C3-2 : +5
+    C4-Wiper ( Arduino A0)   
+       
+    2560
+     __________________________________________
+    /   
+    |
+    |     C1/4  C2/3
+    |
+    |     C3/4
+    |
+    |           C4/4
+    |
+    |     C5/5
+    
+    
+    C1 = Rocker
+    C1-1 : Gnd
+    C1-2 : +5
+    C1-3 : Rocker Wiper ( Arduino A0 )
+    C1-4 : NC
+    
+    C2 = Reset Input
+    C2-1 : Gnd
+    C2-2 : +5
+    C2-3 : ( Arduino D12 )    
+    
+    C3 = LCD
+    C3-1 : Gnd
+    C3-2 : Red
+    C3-3 : SDA ( i2c data )
+    C3-4 : SCL ( i2c clock )
+
+    C4 = motor controller output
+    C4-1 : Nc
+    C4-2 : Pot Gnd
+    C4-3 : Pot Wpr
+    C4-4 : Pot Pwr
+    
+    
+    C5 = Hall Effect Input
+    C5-1 : Gnd
+    C5-2 : +5
+    C5-3 : Hall Input A ( Arduino D2 )
+    C5-4 : Hall Input B (    "    D3 )
+    C5-5 : Hall Input C (    "    D4 )
+    
+    DS1809 UC = D18
+           DC = D17
+*/
+
 // hall effect pin assignemnts
 const int HALL_A = 4;
 const int HALL_B = 5;
@@ -10,7 +74,7 @@ const int HALL_C_BIT = 2;
 const int MOTOR_BRAKE = 4;
 const int SPEED_PWN = 5;
 // variable resistor input for motor speed control
-const int SPEED_INPUT = 0;
+const int SPEED_INPUT = 0; 
 // min and max values for analog read of motor control rocker switch pot
 const int MIN_ROCKER = 0;  
 const int MAX_ROCKER = 1023;
